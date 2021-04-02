ceea4818687aa6682a9f44a8dd5e319137fdde02
Carlga <47149362+Carlga@users.noreply.github.com>
2021-04-02 17:14:00 +0800

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -18,8 +18,9 @@ class Weather:
         with open(os.path.join(os.path.dirname(__file__), 'city.json'), "r", encoding="utf-8") as city_file:
             city_map = json.loads(city_file.read())
         city_code = city_map.get(self.check_item, "101280701")
-        weather_url = f"http://t.weather.itboy.net/api/weather/city/{city_code}"
-        today_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        weather_url = f"http://t.weather.itboy.net/api/weather/city/{city_code}"         
+#         today_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
         resp = requests.get(url=weather_url)
         if resp.status_code == 200 and resp.json().get("status") == 200:
             weather_json = resp.json()
@@ -27,6 +28,7 @@ class Weather:
             notice = today_weather.get("notice")
             high = today_weather.get("high")
             low = today_weather.get("low")
+            today_time = weather_json.get("time")
             temperature = f"温度: {low[low.find(' ') + 1:]}/{high[high.find(' ') + 1:]}"
             wind = f"{today_weather.get('fx')}: {today_weather.get('fl')}"
             aqi = f"空气: {today_weather.get('aqi')}"
